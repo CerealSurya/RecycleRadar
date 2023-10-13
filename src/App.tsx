044f331c102175a387ceedcc22fdf3a5ae1a46c8
LucaDOrso <lucadorso04@gmail.com>
2023-10-13 14:33:59 -0500

    setup config

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import {Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import { styles } from "./styles"
+import LinearGradient from 'react-native-linear-gradient';
+import React from 'react';
 
-export default function App(){
+
+export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View> <LinearGradient
+        colors={['rgba(255,0,150,1)', 'rgba(0,204,255,1)']}
+        style={styles.modernButton}
+      >
+        <Text style={styles.modernButtonText}>Click Me</Text>
+      </LinearGradient>
+      </View>
     </View>
+
   );
 }
+
 
--- src/styles.tsx
+++ src/styles.tsx
@@ -1,9 +1,23 @@
-import { StyleSheet} from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
 
 export const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modernButton: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 15,
+    paddingHorizontal: 32,
+    borderRadius: 12,
+  },
+  modernButtonText: {
+    fontSize: 16,
+    color: 'white',
+  },
 });
+
