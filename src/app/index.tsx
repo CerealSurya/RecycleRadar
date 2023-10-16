d735f5ae4446565c62f32ab2dcc8ffde95db09f8
LucaDOrso <lucadorso04@gmail.com>
2023-10-15 20:03:59 -0500

    setting up icon taskbar

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -24,16 +24,6 @@ export default function App() {
       </Link>
       <Button title="Create Cleanup" onPress={() => navigation.push("/createPost")} />
       <MainContainer />
-      // the tabs
-      {/* <NavigationContainer>
-        <TabNavigator />
-      </NavigationContainer> */}
-      {/* <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen name="Home" component={HomeScreen} />
-          <Tab.Screen name="Settings" component={SettingsScreen} />
-        </Tab.Navigator>
-      </NavigationContainer> */}
     </View>
   );
 }
