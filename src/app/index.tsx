92239700a9f46233e2f6cc88a2015c512f3e4aa7
CerealSurya <sjalapati42@gmail.com>
2023-10-21 10:50:57 -0700

    removed unneccary code in index

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,7 @@
-import { Redirect, useRouter, useFocusEffect } from "expo-router";
-import { Text, View, Button, Pressable } from "react-native";
-import { styles } from "./styles"
 import React from 'react';
 import MainContainer from './MainContainer';
-import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
-  const router = useRouter();
 
   // if user isn't logged in it redirects person to login screen
   // const { user } = useAuth();
